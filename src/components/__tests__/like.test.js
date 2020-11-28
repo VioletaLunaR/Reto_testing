e48ee629f973b75c228670ec4e3f61ac1a85556b
VioletaLunaR <vl.rodriguez10@uniandes.edu.co>
2020-11-28 10:50:06 -0500

    Listo reto 3

--- src/components/__tests__/like.test.js
+++ src/components/__tests__/like.test.js
@@ -27,10 +27,9 @@ describe("Testing Like component", () => {
 
     it("Defaults to 1 paragraph", () => {
         const p = container.querySelector("p");
-        console.log(p.textContent.split(':')[1])
         expect(p.textContent.split(':')[1].trim()).toBe("0");
     });
-    it("Like paragraph increments when button is clicked ", () => {
+    it("Like paragraph increments when button increment is clicked ", () => {
         const button= container.querySelector("#increment");
         const p = container.querySelector("p");
         likes=parseInt(p.textContent.split(':')[1].trim(), 10);
@@ -38,7 +37,26 @@ describe("Testing Like component", () => {
             button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
         });
         expect(parseInt(p.textContent.split(':')[1].trim(), 10)).toBe(likes+1);
-;
     });
+    
+    it("Like paragraph decrements when button decrement is clicked ", () => {
+        const button= container.querySelector("#decrement");
+        const p = container.querySelector("p");
+        likes=parseInt(p.textContent.split(':')[1].trim(), 10);
+        console.log(likes);
+        act(() => {
+            button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+        console.log(p.textContent.split(':')[1].trim());
+        //Nota: en el componente no se válida que el número de likes no sea negativo.
+        //Se deberia matener en cero, si no hay likes y en la pruba se hace un condicional,
+        //en este se mira que si la varibles likes es 0, al presionar decrement sigue siendo 0,
+        //si no es cero, se hace la prueba como está a continuación. 
+        //
+        //Otra forma, sería que el botón decrement estuviera desabilitado cuando el número de likes es 0. 
+        //En la pruba se mirara que si la variable likes es cero, el botón esté desabilidado, y si 
+        //no es cero, se realiza la pruba haciendo el clik verificando como sigue. 
+        expect(parseInt(p.textContent.split(':')[1].trim(), 10)).toBe(likes-1);
+    })
 
 });
